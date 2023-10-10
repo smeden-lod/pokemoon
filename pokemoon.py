2f02399450c590aacbef4146d9f8c600d31ee746
smeden-lod <stefmeden@gmail.com>
2023-10-10 12:41:58 +0200

    update type

--- pokemoon.py
+++ pokemoon.py
@@ -12,20 +12,6 @@ class Pokemoon:
         self.type_plante = type_plante
 
     def fight(self, other):
-        feu = True, False, False
-        eau = False, True, False
-        plante = False, False, True
-        éfficatité = {
-        (feu , plante) : True, #renvoie True si il y a éfficacité de type sinon     False
-        (feu , eau) : False,
-        (feu , feu) : False,
-        (plante , plante) : False,
-        (plante , eau) : True,
-        (plante , feu) : False,
-        (eau , plante) : False,
-        (eau , eau) : False,
-        (eau , feu) : True
-        }
         input(f"Au tour de {self.nom}")
         action = int(input("coup d'boule(1) , charge(2) , sac(3)  "))# attaque spécifique au type(2)
         if action == 1 :
